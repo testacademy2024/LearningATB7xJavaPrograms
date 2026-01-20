659376db9a931437ec5fd07333086f37e1993c2e
testacademy2024 <sanketumrani14041992@gmail.com>
2026-01-20 23:09:41 +0530

    This Week Task - Week#1 - ATBX(20-01-2026 to 25-01-2026)

--- src/ex_20012026/Lab005_ArithmaticException.java
+++ src/ex_20012026/Lab005_ArithmaticException.java
@@ -6,5 +6,9 @@ public class Lab005_ArithmaticException {
         System.out.println(2-2);
         System.out.println(2/2);
         System.out.println(10/0);//Number divided by zeros
+
+        //Right Click on Folder->New->File->Add the content->4lines->Git->Github->Share Projects on Github
+        //Second Time if you want to add a new file then->Add content in class file->4 lines->Git->Commit->Add your message->Committ and push
+
     }
 }
